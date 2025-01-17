1ab11ea88c0eeb9b678cf7af8142fef974bd6b34
Artem Sorokin <102421671+ArtSN7@users.noreply.github.com>
2025-01-17 11:13:41 +0000

    adding list of languages

--- src/components/languages.jsx
+++ src/components/languages.jsx
@@ -1,6 +1,32 @@
 import '../styles/languages.css';
+import deleteIcon from '../data/delete.svg';
+import React, { useState } from 'react';
+
+const lang_list = [
+    {id:crypto.randomUUID(), language: "Spanish", level: "Native"},
+    {id:crypto.randomUUID(), language: "English", level: "Advanced"},
+    {id:crypto.randomUUID(), language: "French", level: "Basic"}
+];
+
+function handleDeleteLanguage(id) {
+    setLanguages(languages.filter(lang => lang.id !== id));
+}
+
+function handleAddLanguage() {
+    if (newLanguage && newLevel) {
+        const newLang = { id: crypto.randomUUID(), language: newLanguage, level: newLevel };
+        setLanguages([...languages, newLang]);
+        setNewLanguage('');
+        setNewLevel('');
+    }
+}
 
 function Languages() {
+
+    const [languages, setLanguages] = useState(lang_list);
+    const [newLanguage, setNewLanguage] = useState('');
+    const [newLevel, setNewLevel] = useState('');
+
 
     return (
         <>
@@ -15,18 +41,24 @@ function Languages() {
                     <option value="native">Native</option>
                 </select>
                 </div>
-                <button type="button">Add New Language</button>
+                <button type="button" onClick={handleAddLanguage}>Add New Language</button>
             </form>
         </div>
-        <div class="form-container">
+        <div className="form-container">
+                {languages.map(lang => (
 
-            <h1>Your data:</h1>
-
-            <p>Spanish - Advanced</p>
-
+                    <div className="list"key={lang.id}>
+                        <p1>{lang.language} - {lang.level}</p1>
+                        <button id="delete_btn" onClick={() => handleDeleteLanguage(lang.id)}>
+                            <img src={deleteIcon} alt="Delete Icon" width="12" height="12" />
+                        </button>
+                    </div>
+                
+                ))}
         </div>
         </>
     );
 }
+
 
 export default Languages;
